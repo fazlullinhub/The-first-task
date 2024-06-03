4f9163e641cf87a859903a64662965cbe4e342b6
fazlullinhub <164046064+fazlullinhub@users.noreply.github.com>
2024-06-03 21:56:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number from 0 to 9: ");
+            string input = Console.ReadLine();
+            try
+            {
+                int number = Convert.ToInt32(input);
+                if(number < int.MinValue || number > int.MaxValue)
+                {
+                    throw new OverflowException("Number has gone out of bounds");
+                }
+                Console.WriteLine("The converted number" + number);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("It's not a number");
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
